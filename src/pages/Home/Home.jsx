b5cf1c43f00b1fa35fe32e852f2a7b867c17bdef
See-Monkey <226230446+See-Monkey@users.noreply.github.com>
2026-03-29 19:39:26 -0600

    Setup home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,1 +1,9 @@
-export default function Home() {}
+import styles from "./Home.module.css";
+
+export default function Home() {
+	return (
+		<section className={styles.homeSection}>
+			<h1>Home Page</h1>
+		</section>
+	);
+}
